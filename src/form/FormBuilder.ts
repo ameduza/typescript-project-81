c11d872c574a2b2b544519e3ad8216b0d51bbecd
ameduza <ameduza@gmail.com>
2026-08-17 09:53:26 +0200

    Fix linting issue

--- src/form/FormBuilder.ts
+++ src/form/FormBuilder.ts
@@ -15,7 +15,7 @@ export const TEXTAREA_DEFAULTS = { cols: 20, rows: 40 } as const;
  */
 function capitalizeFirstChar(str: string): string {
   if (str.length === 0) return str;
-  return str[0]!.toUpperCase() + str.slice(1);
+  return str[0].toUpperCase() + str.slice(1);
 }
 
 /**
